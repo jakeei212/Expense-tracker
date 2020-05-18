f66a3650c3deabd20e01820d81662b9038855918
jakeei212 <53650114+jakeei212@users.noreply.github.com>
2020-05-18 11:33:31 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,7 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyBk532DVzF_COJlgYSTnQ13auL964V0po0",
-  authDomain: "nba-app-c83aa.firebaseapp.com",
-  databaseURL: "https://nba-app-c83aa.firebaseio.com",
-  projectId: "nba-app-c83aa",
-  storageBucket: "nba-app-c83aa.appspot.com",
-  messagingSenderId: "368883105837",
-  appId: "1:368883105837:web:051b6ce73780af8d6138a4",
-  measurementId: "G-J1CYW3XNRG",
+
 };
 
 firebase.initializeApp(config);
